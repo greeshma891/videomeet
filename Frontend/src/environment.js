4aa84bd385198c7db7b59a1f4d61234a9236cb4b
greeshma891 <greeshmaaitha120@gmail.com>
2026-01-01 23:22:01 +0530

    Deploying frontend

--- Frontend/src/environment.js
+++ Frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    https://videomeetprojectbackend.onrender.com :
+    https://videomeetprojectbackend.onrender.com ;
 
     "http://localhost:8080"
 
